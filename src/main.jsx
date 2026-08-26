d52e2693128446c64e62d98968aec07ff570b958
callz2smart <leviorourke23@icloud.com>
2026-08-26 20:15:06 +1000

    Build MM2Wild header in React

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource/rubik/latin-400.css";
+import "@fontsource/rubik/latin-500.css";
+import "@fontsource/rubik/latin-700.css";
+import "@fontsource/rubik/latin-800.css";
+import App from "./App";
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
